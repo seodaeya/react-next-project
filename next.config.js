f82122ea2c6d5287f56d6e6df060671991b32aea
Nard <seodaeya@gmail.com>
2023-07-21 13:45:25 +0900

    axios sample 추가 api 서버는 간단하게 만들어서 올릴 예정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: 'http://localhost:8090/:path*',
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
---
+++ pages/main.tsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import {useState} from "react";
+
+/**
+ * axios를 배운다.
+ *
+ * @constructor
+ */
+export default function Main() {
+
+    const [getResult, setGetResult] = useState('');
+    const [postResult, setPostResult] = useState('');
+
+    /**
+     * axios get
+     */
+    function getAxios() {
+        const params = new URLSearchParams({
+            'param1':'value1',
+            'param2':'value2'
+        });
+        axios.get('/test/get' + (params ? `?${params.toString()}` : ''))
+        .then(res => {
+            console.log(res);
+            const {data} = res.data;
+            setGetResult(data);
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    /**
+     * axios post
+     */
+    function post() {
+        let params = {
+            param1: 'value1',
+            param2: 'value2'
+        }
+        axios.post('/test/post', params)
+        .then(res => {
+            console.log(res);
+            const {data} = res.data;
+            setPostResult(data);
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }
+
+    return <div>
+        <h1>Hello, Axios!</h1>
+        <div>
+            <button onClick={getAxios}>get</button>
+            <p>{getResult}</p>
+        </div>
+        <div>
+            <button onClick={post}>post</button>
+            <p>{postResult}</p>
+        </div>
+    </div>
+}
