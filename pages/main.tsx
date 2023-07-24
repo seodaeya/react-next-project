c1f0ffac9ff5332339a615b03ec0144156f4bfe4
Nard <seodaeya@gmail.com>
2023-07-24 11:15:15 +0900

    엑셀 업로드 샘플 추가

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import {useState} from "react";
 
+import { FileUploader } from 'react-drag-drop-files';
+
 /**
  * axios를 배운다.
  *
@@ -8,13 +10,20 @@ import {useState} from "react";
  */
 export default function Main() {
 
+    // get
     const [getResult, setGetResult] = useState('');
+    // post
     const [postResult, setPostResult] = useState('');
+    // excel uploads
+    const [file, setFile] = useState(null);
+    const handleChange = (pFile: any) => {
+        setFile(pFile);
+    };
 
     /**
      * axios get
      */
-    function getAxios() {
+    const get = () => {
         const params = new URLSearchParams({
             'param1':'value1',
             'param2':'value2'
@@ -33,7 +42,7 @@ export default function Main() {
     /**
      * axios post
      */
-    function post() {
+    const post = () => {
         let params = {
             param1: 'value1',
             param2: 'value2'
@@ -49,13 +58,40 @@ export default function Main() {
         })
     }
 
+    const uploadFile = () => {
+
+        const headers = {
+            'Content-type': 'multipart/form-data',
+        }
+        axios.post('/api/file', {'file': file}, {headers})
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((e) => {
+            console.error(e);
+        });
+    };
+
+
     return <div>
         <h1>Hello, Axios!</h1>
         <div>
-            <button onClick={getAxios}>get</button>
+            <h2>To Drag & Drop Excel Files</h2>
+            <FileUploader
+                name="file"
+                types={['xlsx']}
+                handleChange={handleChange}
+            />
+            <p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+            <button onClick={uploadFile}>엑셀 업로드</button>
+        </div>
+        <div>
+            <h2>Get</h2>
+            <button onClick={get}>get</button>
             <p>{getResult}</p>
         </div>
         <div>
+            <h2>Post</h2>
             <button onClick={post}>post</button>
             <p>{postResult}</p>
         </div>
